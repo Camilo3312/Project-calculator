0e8119afe1e3b882b4aa777fbf6512898cc62fc3
camilo3312 <85415169+camilo3312@users.noreply.github.com>
2021-08-11 17:45:42 -0500

    Update calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,27 @@
+print("Calculadora ")
+print("Ingrese un numero para hacer las operaciones")
+print("(1) Suma")
+print("(2) Resta")
+print("(3) Multiplicación")
+print("(4) División")
+
+switch = True;
+
+while (switch == True):
+
+    opcion = int(input("Ingrese un numero: "))
+
+    Numero1 = 0
+
+    Numero2 = 0
+      
+    if opcion == 1:
+    
+    elif opcion == 2:
+
+    elif opcion == 3:
+
+    elif opcion == 4:
+
+    else:
+        switch = False
